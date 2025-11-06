8363d0eb60c75398b0fd29bad8f30ba386b225cd
Chrish1079 <itssuper1079@gmail.com>
2025-11-05 22:53:50 -0500

    submit

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import ProductList from './components/ProductList';
 
 const App = () => {
   // TODO: Define initial product data
+  const initialProducts = [
+    { id: 1, name: 'Laptop', price: '$999', inStock: true },
+    { id: 2, name: 'Phone', price: '$699', inStock: false },
+    { id: 3, name: 'Tablet', price: '$499', inStock: true },
+  ];
 
   // TODO: Implement state to manage filtering
+  const [products, setProducts] = useState(initialProducts);
 
   // TODO: Implement logic to filter products based on availability
 
+  const handleRemove = (productId) => {
+    setProducts(products.filter(product => product.id !== productId));
+  };
+
   return (
     <div>
-      <h1>{/* TODO: Add 'Product Dashboard' title here */}</h1>
-      
-      {/* TODO: Add buttons to allow filtering by availability */}
-
-      {/* TODO: Render the ProductList component and pass filtered products */}
-      
+      <h1>Product Dashboard</h1>
+      <ProductList products={products} onRemove={handleRemove} />
     </div>
   );
 };
